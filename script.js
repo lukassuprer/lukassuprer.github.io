4af198a4a1de42a338f8b5773c71a801ada71f0c
lukassuper <lukaswedry@seznam.cz>
2022-04-19 17:54:50 +0200

    navbar

--- script.js
+++ script.js
@@ -7,54 +7,85 @@ const buttonNavbar = document.querySelector('.buttonNavbar');
 const crossIcon = document.querySelector('#crossIcon');
 const navbarSmall = document.querySelector('.navbarSmall');
 const heading = document.querySelector('#heading');
+const textNavbar = document.querySelectorAll('.navbarText')
 
 setPosition(0);
 setInterval(checkPos, 1);
 buttonNavbar.addEventListener('click', navbarButtonClick);
+console.log(textNavbar.length);
+
+function animateNavBarIn() {
+    navbar.classList.remove('notMoved');
+    navbar.classList.add('moved');
+    navbar.animate([
+        { transform: 'translateX(-2000px)' },
+        { transform: 'translateX(0px)' }
+    ], {
+        duration: 500,
+        iterations: 1
+    });
+    navbar.classList.remove('moved');
+    navbar.classList.add('notMoved');
+}
+
+function animateNavBarOut() {
+    navbar.classList.remove('moved');
+    navbar.classList.add('notMoved');
+    navbar.animate([
+        { transform: 'translateX(0px)' },
+        { transform: 'translateX(-2000px)' },
+    ], {
+        duration: 1000,
+        iterations: 1
+    });
+    navbar.classList.remove('notMoved');
+    navbar.classList.add('moved');
+    navbar.style.animation = "mymove";
+}
 
 
-function setPosition(numberOfPosition){
+function setPosition(numberOfPosition) {
+    //animateNavBarOut();
     let position = arrayOfPositions[numberOfPosition];
-    navbar.style.top = position.getBoundingClientRect().top +  "px";
+    navbar.style.top = position.getBoundingClientRect().top + "px";
     navbar.style.left = position.getBoundingClientRect().left + "px";
     navbar.style.width = position.offsetWidth + "px";
     navbar.style.height = position.offsetHeight + "px";
-    // navbar.style.position = "relative";
     navbar.style.zIndex = "1";
     position.appendChild(navbar);
-    navbar.classList.toggle("active");
+    animateNavBarIn();
 }
-function checkPos(){
+function checkPos() {
     const rect = navbar.getBoundingClientRect();
-    if(rect.top < 0){
-        if(posNumber < arrayOfPositions.length - 1){
+    if (rect.top < 0) {
+        if (posNumber < arrayOfPositions.length - 1) {
             posNumber++;
             setPosition(posNumber)
         }
-        else{
+        else {
             posNumber = 0;
             setPosition(posNumber)
         }
     }
-    else if(rect.top > 570){
-        if(posNumber > 0){
+    else if (rect.top > 570) {
+        if (posNumber > 0) {
             posNumber--;
             setPosition(posNumber)
         }
-        else{
+        else {
             posNumber = arrayOfPositions.length - 1;
             setPosition(posNumber)
         }
     }
 }
 
-function navbarButtonClick(){
-    if(navbarSmall.style.visibility == "visible"){
+function navbarButtonClick() {
+    if (navbarSmall.style.visibility == "visible") {
         navbarSmall.classList.toggle('active');
         buttonNavbar.classList.toggle('active');
         navbarSmall.classList.add('hidden');
     }
-    else{
+    else {
         navbarSmall.classList.toggle('active');
         buttonNavbar.classList.toggle('active');
         navbarSmall.classList.remove('hidden');
