fc02a460e9765d5554cb8c61a71dd01ee7e4caa7
lukassuper <lukaswedry@seznam.cz>
2022-04-30 10:40:06 +0200

    shit

--- FINAL WORK MADAFAKA/script.js
+++ FINAL WORK MADAFAKA/script.js
@@ -7,7 +7,8 @@ const buttonNavbar = document.querySelector('.buttonNavbar');
 const crossIcon = document.querySelector('#crossIcon');
 const navbarSmall = document.querySelector('.navbarSmall');
 const heading = document.querySelector('#heading');
-const textNavbar = document.querySelectorAll('.navbarText')
+const textNavbar = document.querySelectorAll('.navbarText');
+const textSmallNavbar = document.querySelectorAll('.navbarSmallText');
 const body = document.getElementById('body');
 let navbarIsVisible = false;
 
@@ -17,6 +18,7 @@ let evenImages = document.querySelectorAll('.evenImage');
 let oddImages = document.querySelectorAll('.oddImage');
 let nightImages = [];
 const myImageCount = 3;
+let nextImage = 0;
 
 setPosition(0);
 setInterval(checkPos, 1);
@@ -46,20 +48,24 @@ function imageInterval() {
             { opacity: 1 },
             { opacity: 0 }
         ], {
-            duration: 3000,
+            duration: 2000,
             iterations: 1
         });
         animation.addEventListener('finish', function () {
-            nightImages[i].style.backgroundImage = images[Math.floor(Math.random() * ((j + myImageCount) - j) + j)];
+            nightImages[i].style.backgroundImage = images[j + nextImage];
             j += myImageCount;
             nightImages[i].animate([
                 { opacity: 0 },
                 { opacity: 1 }
             ], {
-                duration: 3000,
+                duration: 2000,
                 iterations: 1
             });
         });
+    }
+    nextImage++;
+    if (nextImage >= myImageCount) {
+        nextImage = 0;
     }
 }
 function animateNavBarIn() {
@@ -77,6 +83,13 @@ function animateNavBarIn() {
 }
 
 function setPosition(numberOfPosition) {
+    if (numberOfPosition > textNavbar.length - 1) {
+        textNavbar[numberOfPosition - 1].style.color = 'red';
+        textSmallNavbar[numberOfPosition - 1].style.color = 'red';
+    } else {
+        textNavbar[numberOfPosition].style.color = 'red';
+        textSmallNavbar[numberOfPosition].style.color = 'red';
+    }
     let position = arrayOfPositions[numberOfPosition];
     navbar.style.top = position.getBoundingClientRect().top + "px";
     navbar.style.left = position.getBoundingClientRect().left + "px";
@@ -90,23 +103,39 @@ function checkPos() {
     const rect = navbar.getBoundingClientRect();
     if (rect.top < 0) {
         if (posNumber < arrayOfPositions.length - 1) {
+            textNavbarHighlight(posNumber);
             posNumber++;
             setPosition(posNumber)
         }
         else {
+            textNavbarHighlight(posNumber);
             posNumber = 0;
             setPosition(posNumber)
         }
     }
     else if (rect.top > 570) {
         if (posNumber > 0) {
+            textNavbarHighlight(posNumber);
             posNumber--;
             setPosition(posNumber)
         }
         else {
+            textNavbarHighlight(posNumber);
             posNumber = arrayOfPositions.length - 1;
             setPosition(posNumber)
         }
+    }
+}
+
+function textNavbarHighlight(numberOfPosition) {
+    if (numberOfPosition > textNavbar.length - 1) {
+        numberOfPosition = textNavbar.length - 1;
+        textNavbar[numberOfPosition].style.color = 'white';
+        textSmallNavbar[numberOfPosition].style.color = 'white';
+    }
+    else {
+        textNavbar[numberOfPosition].style.color = 'white';
+        textSmallNavbar[numberOfPosition].style.color = 'white';
     }
 }
 
